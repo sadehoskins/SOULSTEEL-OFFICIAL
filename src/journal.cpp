312ddc403b18c9f986f0453be7c2ff093258c380
Lara <152970053+LaraRF@users.noreply.github.com>
2024-09-11 17:11:57 +0200

    new journal navigation and content

--- src/journal.cpp
+++ src/journal.cpp
@@ -7,40 +7,18 @@
 #include "mainmenu.h"
 
 void journal::update() {
-    //navigating the journal by using arrow keys
-    if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {
-        if (cursor == 2) {
-            cursor = 2; //ends with last box, does not come back on the other side of the screen
-        } else {
-            cursor++;
-        }
-    }
-
-    if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) {
-        if (cursor == 0) {
-            cursor = 0; //same here: you can't go more the side if you're already on the last box
-        } else {
-            cursor--;
-        }
-    }
+    journalnavigation();
+    flippingpages();
 }
 
 scene *journal::evaluateSceneChange() {
     if (IsKeyPressed(KEY_T)) {
         return gameplayInstance;
-    }else if (IsKeyPressed(KEY_M)) {
+    } else if (IsKeyPressed(KEY_M)) {
         return new mainmenu();
-    } else if (IsKeyPressed(KEY_ENTER)) {
-        switch (cursor) {
-            case 0:
-                //no journal content yet
-                break;
-            case 1:
-                //no journal content yet
-                break;
-            case 2:
-                return gameplayInstance;
-        }
+    } else if (closejournal) {
+        closejournal = false;  // Reset the bool
+        return gameplayInstance;
     } else {
         return this;
     }
@@ -49,12 +27,23 @@ scene *journal::evaluateSceneChange() {
 void journal::draw() {
     DrawTexture(journalbackground, 5, 5, WHITE);
 
-    switch (cursor) { //shows which box is selected by giving it a white outline
+    switch(page){
         case 0:
-            DrawRectangleRec(journalpage1_marked, WHITE);
+            drawpage0();
             break;
         case 1:
-            DrawRectangleRec(journalpage2_marked, WHITE);
+            drawpage1();
+            break;
+        case 2:
+            drawpage2();
+            break;
+    }
+}
+
+void journal::drawpage0() {
+    switch (cursor) { //shows which box is selected by giving it a white outline
+        case 1:
+            DrawRectangleRec(arrowforward_marked, WHITE);
             break;
         case 2:
             DrawRectangleRec(hitbox_close_marked, WHITE);
@@ -62,21 +51,153 @@ void journal::draw() {
         default:
             break;
     }
-
-
-    DrawRectangleRec(journalpage1, GRAY);
-    DrawRectangleRec(journalpage2, GRAY);
+    DrawRectangleRec(arrowforward, GRAY);
     DrawRectangleRec(hitbox_close, GRAY);
 
     DrawTexture(closeButton, 685, 395, WHITE);
 
-    DrawText("Journal content TBD", 45, 20, 23, BLACK);
-    DrawText("Journal content TBD", 395, 20, 23, BLACK);
-    DrawText("Eventually here\nwill be a journal\nwith different pages.", 60, 200, 20, BLACK);
-    DrawText("At this time\nnothing happens on\nthese two pages.", 410, 200, 20, BLACK);
-
+    DrawText("DAY 1", 32*4,20,25,BLACK);
+    DrawText(day1text, 40, 70, 20, BLACK);
+    DrawText("DAY 2", 32*12+16+32*3, 20, 25, BLACK);
+    DrawText(day2text, 32*13, 70, 20, BLACK);
 }
+
+void journal::drawpage1() {
+    switch (cursor) { //shows which box is selected by giving it a white outline
+        case 0:
+            DrawRectangleRec(arrowback_marked, WHITE);
+            break;
+        case 1:
+            DrawRectangleRec(arrowforward_marked, WHITE);
+            break;
+        case 2:
+            DrawRectangleRec(hitbox_close_marked, WHITE);
+            break;
+        default:
+            break;
+    }
+    DrawRectangleRec(arrowforward, GRAY);
+    DrawRectangleRec(hitbox_close, GRAY);
+    DrawRectangleRec(arrowback,GRAY);
+
+    DrawTexture(closeButton, 685, 395, WHITE);
+
+    DrawText("DAY 3", 32*4,20,25,BLACK);
+    DrawText("The strange occurrences\ncontinue. A janitor\nreported seeing a living\ntoy, and the power\nin the factory keeps going\nout intermittently. It's almost\nas if the factory is\ncursed, or someone is\ntampering with the\ngenerator. I'm starting to\nget worried...", 40, 70, 20, BLACK);
+}
+
+void journal::drawpage2() {
+    switch (cursor) { //shows which box is selected by giving it a white outline
+        case 0:
+            DrawRectangleRec(arrowback_marked, WHITE);
+            break;
+        case 1:
+            DrawRectangleRec(arrowforward_marked, WHITE);
+            break;
+        default:
+            break;
+    }
+
+    DrawRectangleRec(hitbox_close, GRAY);
+    DrawRectangleRec(arrowback,GRAY);
+
+    DrawTexture(closeButton, 685, 395, WHITE);
+
+    DrawText("DAY 5", 32*4,20,25,BLACK);
+    DrawText("The strange occurrences\ncontinue. A janitor\nreported seeing a living\ntoy, and the power\nin the factory keeps going\nout intermittently. It's almost\nas if the factory is\ncursed, or someone is\ntampering with the\ngenerator. I'm starting to\nget worried...", 40, 70, 20, BLACK);
+}
+
 
 void journal::drawDebug() {
 
 }
+
+void journal::journalnavigation() {
+//navigating the journal by using arrow keys
+
+switch(page){
+    case 0:
+        if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {
+            if (cursor == 2) {
+                cursor = 2; //ends with last box, does not come back on the other side of the screen
+            } else {
+                cursor++;
+            }
+        }
+
+        if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) {
+            if (cursor == 1) {
+                cursor = 1; //same here: you can't go more the side if you're already on the last box
+            } else {
+                cursor--;
+            }
+        }
+        if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
+            cursor = 2;
+        }
+        if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
+            cursor = 1;
+        }
+        break;
+    case 1:
+        if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {
+            if (cursor == 2) {
+                cursor = 2; //ends with last box, does not come back on the other side of the screen
+            } else {
+                cursor++;
+            }
+        }
+
+        if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) {
+            if (cursor == 0) {
+                cursor = 0; //same here: you can't go more the side if you're already on the last box
+            } else {
+                cursor--;
+            }
+        }
+        if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
+            cursor = 2;
+        }
+        if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
+            cursor = 1;
+        }
+        break;
+    case 2:
+        if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {
+            cursor=2;
+        }
+
+        if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) {
+            cursor=0;
+        }
+        if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) {
+            cursor = 2;
+        }
+        if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) {
+            cursor = 0;
+        }
+        break;
+}
+}
+
+void journal::flippingpages() {
+
+    if (IsKeyPressed(KEY_ENTER)) {
+        switch (cursor) {
+            case 0:  //arrow to the left
+                if (page > 0) {
+                    page--;
+                }
+                break;
+            case 1:  //arrow to the right
+                if (page < 2) {
+                    page++;
+                }
+                break;
+            case 2:  // Close button
+                closejournal = true;
+                break;
+        }
+    }
+}
+
--- src/journal.h
+++ src/journal.h
@@ -17,7 +17,8 @@ public:
             std::cout << "Warning: gameplayInstance is null in ingameoptions constructor\n";
         }
     }
-    int cursor = 0;
+    int cursor = 1;
+    int page=0;
 
     void update() override;
 
@@ -27,16 +28,29 @@ public:
 
     void drawDebug() override;
 
+    void journalnavigation();
+
+    void flippingpages();
+
+    bool closejournal = false;
+
+    void drawpage0();
+    void drawpage1();
+    void drawpage2();
+
+    const char *day1text ="The experiments are going\nwell. The energy retention\nthrough the souls is\noutstanding, although\nI initially had my\nconcerns since we know so\nlittle about souls.";
+    const char *day2text ="Today, the devices showed\nerrors even though there\nwere none. The equipment\nis supposed to be up\nto date, so this shouldn't\nbe happening. Is it a\nbad omen or just a\ncoincidence...?";
+
     //creates the boxes that will be used as buttons and loads the textures; marked boxes will work as an outline to show which button is selected
     Rectangle hitbox_close = {670, 400, 100, 60};
     Rectangle hitbox_close_marked = {660, 390, 120, 80};
     Texture2D closeButton = assestmanagergraphics::getTexture("userinterface/button_close");
 
-    Rectangle journalpage1 = {25, 70, 280, 350};
-    Rectangle journalpage1_marked = {15, 60, 300, 370};
+    Rectangle arrowback = {16, 32*7+16, 32, 32};
+    Rectangle arrowback_marked = {6, 32*7+6, 52, 52};
 
-    Rectangle journalpage2 = {375, 70, 280, 350};
-    Rectangle journalpage2_marked = {365, 60, 300, 370};
+    Rectangle arrowforward = {32*24-16, 32*7+16, 32, 32};
+    Rectangle arrowforward_marked = {32*24-26, 32*7+6, 52, 52};
 
     Texture2D journalbackground = assestmanagergraphics::getTexture("userinterface/journal_background_newsize");
 
